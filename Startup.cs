57222877805276e2aa7424217668f7aebdb7dee6
Konstantin Fukszon <kofukszo@microsoft.com>
2019-10-15 17:08:41 +0200

    minor change

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace az203_demo_webapp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!");
                 });
             });
         }
